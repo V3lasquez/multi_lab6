0cfeca222f5e51508146208fc6eec9aed347e682
V3lasquez <noe.vc1782@gmail.com>
2023-10-14 21:18:44 -0500

    Agregar archivos de proyecto.

---
+++ multi_lab6/multi_lab6/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewExample
+{
+    public class Item
+    {
+        public string Nombre { get; set; }
+        public string Dni { get; set; }
+    }
+}
---
+++ multi_lab6/multi_lab6/ListViewGroup.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewGroup : ContentPage
+    {
+        public ObservableCollection<Group> Groups { get; set; }
+        public ListViewGroup()
+        {
+            InitializeComponent();
+            Groups = new ObservableCollection<Group>
+        {
+            new Group("A")
+            {
+                new Item { Nombre = "Alfredo", Dni = "Pika" },
+                new Item { Nombre = "Angel", Dni = "Trebol" },
+                new Item { Nombre = "Alonso", Dni = "Corazon" },
+            },
+            new Group("B")
+            {
+                new Item { Nombre = "Bruno", Dni = "Play" },
+                new Item { Nombre = "Bernardo", Dni = "Xbox" },
+                new Item { Nombre = "Beatriz", Dni = "Nintendo" },
+
+            },
+            new Group("C")
+            {
+                new Item { Nombre = "Carol", Dni = "Java" },
+                new Item { Nombre = "Camila", Dni = "Swift" },
+                new Item { Nombre = "Carmen", Dni = "c#" },
+
+            },
+        };
+            GroupedView.ItemsSource = Groups;
+        }
+    }
+}
